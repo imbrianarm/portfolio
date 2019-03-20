c5083804ff41c09421bcf6d5786460d5410ae7ff
Brian <imbrianarm@gmail.com>
2019-03-19 22:42:43 -0400

    completed hamburger menu

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
 // DEFINE PROJECT APP OBJECT
 const app = {};
 
+app.init = () => {};
+
 $(document).ready(() => {
   app.init();
-});
+  // Smooth scroll on anchor click
+  $('a[href*="#"]').on("click", function(e) {
+    e.preventDefault();
 
-app.init = () => {
-};
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top
+      },
+      250,
+      "linear"
+    );
+  });
 
-// Smooth scroll on anchor click
-$('a[href*="#"]').on("click", function(e) {
-  e.preventDefault();
+  // Smooth scroll on hire me button click
+  $(".aboutButton").on("click", function(e) {
+    e.preventDefault();
 
-  $("html, body").animate(
-    {
-      scrollTop: $($(this).attr("href")).offset().top
-    },
-    250,
-    "linear"
-  );
-});
+    $("html, body").animate(
+      {
+        scrollTop: $("#contact").offset().top
+      },
+      250,
+      "linear"
+    );
+  });
 
-// Smooth scroll on hire me button click
-$(".aboutButton").on("click", function(e) {
-  e.preventDefault();
+  // Hamburger Menu Animation
+  $(".sidebarBtn").click(function() {
+    $(".headerNav").toggleClass("active");
+    $(".sidebarBtn").toggleClass("toggle");
+  });
 
-  $("html, body").animate(
-    {
-      scrollTop: $("#contact").offset().top
-    },
-    250,
-    "linear"
-  );
+  // Close Hamburger on Link click
+  $(".headerNav a").on("click", function() {
+    $(".headerNav").removeClass("active");
+    $(".sidebarBtn").toggleClass("toggle");
+  });
 });
